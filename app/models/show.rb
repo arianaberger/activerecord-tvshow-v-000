d021defb8c770462dc69c2724d8bede9f5550a6a
Ariana Berger <ariana.berger@gmail.com>
2018-04-03 22:35:19 +0000

    automatically backed up by learn

---
+++ app/models/show.rb
@@ -0,0 +1,11 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum("rating")
+  end
+
+  def self.most_popular_show
+    top_rating = self.highest_rating
+    show = self.find_by(rating: top_rating)
+  end
+end
